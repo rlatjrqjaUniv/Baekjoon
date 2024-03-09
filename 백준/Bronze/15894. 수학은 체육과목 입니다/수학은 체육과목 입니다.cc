49bf67531c1a983c4aa41cc21e48be7be561721d
rlatjrqjaUniv <127767484+rlatjrqjaUniv@users.noreply.github.com>
2024-03-09 15:55:35 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -3,15 +3,10 @@ using namespace std;
 
 int main()
 {
-	double num = 0;
-	long long result = 0;
+	long long num = 0;
 	cin >> num;
 
-	//밑면 n개, 왼쪽 면 n개, 오른쪽 면 n개 : 3x1xn
-	//반쪽짜리 윗 면 n-1개 * 2 (양쪽으로 반개씩)
-	//맨 윗면 1
-	result = 3 * (1 * num) + 2 * (0.5 * (num - 1)) + 1;
-	cout << result;
+	cout << num*4;
 
 	return 0;
 }
