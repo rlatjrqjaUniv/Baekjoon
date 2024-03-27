bacfebe4a18cfd8665452d3125ad15757c33bdd0
rlatjrqjaUniv <127767484+rlatjrqjaUniv@users.noreply.github.com>
2024-03-27 23:58:39 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long num = 0, sum = 0;
+    cin >> num;
+    while (num--)
+    {
+        sum += num;
+    }
+    cout << sum << endl << 2;
+}
